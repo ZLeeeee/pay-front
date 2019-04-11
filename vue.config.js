00321edf1e994b8d3a1de7b27d98e33f30e21c85
Administrator <f710334789@163.com>
2019-04-11 11:22:19 +0800

    添加

---
+++ vue.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    baseUrl: process.env.NODE_ENV === "production" ? "./" : "/",
+    devServer: {
+        open: true,
+        host: '127.0.0.1',
+        port: 9999,
+        proxy: {
+            '/xboot': {
+                // target: 'http://127.0.0.1:8888',  // 请求本地 需要xboot后台项目
+                // target: 'http://114.116.93.197:8080',  // 请求本地 需要xboot后台项目
+                target: 'http://localhost:8080',  // 请求本地 需要xboot后台项目
+                ws: true
+            },
+            '/api': {
+                //target: 'http://localhost:8080',
+                target: 'http://192.168.3.56:8080',
+                // target: 'http://114.116.93.197:8080',
+                //target: 'http://114.115.161.193:8080',
+                //target: 'http://7bdd99c1.ngrok.io',
+                pathRewrite: { "^/api": "" }
+            }
+        }
+    },
+    // 打包时不生成.map文件 避免看到源码
+    productionSourceMap: false
+
+}
